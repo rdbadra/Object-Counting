81d2f7daa868b3ea121a711e810791537346fb96
roberto <rdbadra@gmail.com>
2021-08-22 00:00:59 +0200

    images with overlapping correctly created

--- lcfcn/divide_mosaic.py
+++ lcfcn/divide_mosaic.py
@@ -9,8 +9,8 @@ IMAGE_DIR = '/home/roberto/Descargas/ImagenesMicroplasticos/original/'
 CLICKS_DIR = '/home/roberto/Descargas/ImagenesMicroplasticos/images_clicks/'
 POINTS_DIR = '/home/roberto/Descargas/ImagenesMicroplasticos/points/'
 MOSAIC_DIR = '/home/roberto/Descargas/ImagenesMicroplasticos/mosaic/'
-JUMP = 800
-OVERLAP = 0
+JUMP = 600
+OVERLAP = 200
 
 def has_points(h_start, h_end, w_start, w_end, clicks):
     for click in clicks:
@@ -22,12 +22,13 @@ def generate_dots(h_start, h_end, w_start, w_end, clicks, mosaic_image):
     dots = np.zeros((mosaic_image.shape[0], mosaic_image.shape[1], 3), np.uint8)
     for click in clicks:
         if click[1] <= h_end and click[1] >= h_start and click[0] <= w_end and click[0] >= w_start:
-            h_int = int(click[1] / (JUMP))
-            w_int = int(click[0] / (JUMP))
+            h_int = int(click[1] / (JUMP + OVERLAP))
+            w_int = int(click[0] / (JUMP + OVERLAP))
             #dots = cv2.circle(output, (int(x * (224 / img.shape[1])), int(y * (224 / img.shape[0]))), radius=0, color=(0, 0, 255), thickness=1)
             x = (click[0] - ((JUMP) * w_int))
             y = (click[1] - ((JUMP) * h_int))
-            dots = cv2.circle(dots, (x, y), radius=0, color=(0, 0, 255), thickness=1)
+            print(f'Click: x: {click[0]} | y: {click[1]} ======= h_start: {h_start} | h_end: {h_end} | w_start: {w_start} | w_end: {w_end} | x: {x} | y: {y} | h_int: {h_int} | w_int: {w_int}')
+            dots = cv2.circle(dots, (x, y), radius=1, color=(0, 0, 255), thickness=1)
     return dots
 
 
@@ -52,10 +53,8 @@ for file in images:
                 cv2.imwrite(f'{MOSAIC_DIR}{file}_{h}_{w}.png', image[h:h+(JUMP + OVERLAP), w:w+(JUMP + OVERLAP), :])
                 dots = generate_dots(h, h+(JUMP + OVERLAP), w, w+(JUMP + OVERLAP), clicks, image[h:h+(JUMP + OVERLAP), w:w+(JUMP + OVERLAP), :])
                 cv2.imwrite(f'{MOSAIC_DIR}{file}_{h}_{w}_dots.png', dots)
-                print(f'Original: {image.shape} | Mosaic Image: {image[h:h+(JUMP + OVERLAP), w:w+(JUMP + OVERLAP), :].shape} | dots image: {dots.shape}')
-            '''
-            if w_i == 5:
-                image = cv2.rectangle(image, (h, w), (h+JUMP, w+JUMP), 2, 1)
-                cv2.imwrite(f'{MOSAIC_DIR}file_{h}_{w}.png', image)
+                print(f'Original: {image.shape} | Mosaic Image: {image[h:h+(JUMP + OVERLAP), w:w+(JUMP + OVERLAP), :].shape} | dots image: {dots.shape}' +
+                    f' | Position: ({h}:{h+(JUMP + OVERLAP)}, {w}:{w+(JUMP + OVERLAP)}) | Name: {file}_{h}_{w}_dots.png')
                 break
-            '''
+        break
+    break
---
+++ lcfcn/test_image.py
@@ -0,0 +1,75 @@
+import urllib
+import cv2
+from os import walk
+import numpy as np
+import pickle
+
+IMAGE_DIR = '/home/roberto/Descargas/ImagenesMicroplasticos/'
+
+all_files = []
+for (dirpath, dirnames, filenames) in walk(f'{IMAGE_DIR}mosaic/'):
+    all_files.extend(filenames)
+    break
+
+def mouse_callback(event, x, y, flags, params):
+
+    #right-click event value is 2
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # fontScale
+    fontScale = 8
+    
+    # Blue color in BGR
+    color = (0, 255, 255)
+    
+    # Line thickness of 2 px
+    thickness = 5
+    if event == 1:
+        print([x, y])
+        #file_clicks.append([int(x * (224 / img.shape[1])), int(y * (224 / img.shape[0]))])
+        #store the coordinates of the right-click event
+        '''
+        output = cv2.circle(output, (int(x * (224 / img.shape[1])), int(y * (224 / img.shape[0]))), radius=0, color=(0, 0, 255), thickness=1)
+
+        #this just verifies that the mouse data is being collected
+        #you probably want to remove this later
+        print(file_clicks)
+        img = cv2.putText(img, f'{len(file_clicks)}', (x, y), font, 
+                   fontScale, color, thickness, cv2.LINE_AA)
+        img = cv2.circle(img, (x, y), radius=10, color=(0, 0, 255), thickness=20)
+        cv2.imshow('image', img)
+        '''
+
+for image_name in all_files:
+    print(image_name)
+    file_clicks = []
+    path_image = f'{IMAGE_DIR}mosaic/' + image_name
+    img = cv2.imread(path_image)
+    scale_width = 640 / img.shape[1]
+    scale_height = 480 / img.shape[0]
+    height = img.shape[0]
+    width = img.shape[1]
+    scale = min(scale_width, scale_height)
+    window_width = int(img.shape[1] * scale)
+    window_height = int(img.shape[0] * scale)
+    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('image', window_width, window_height)
+    output = np.zeros((224, 224, 3), np.uint8)
+
+    #set mouse callback function for window
+    cv2.setMouseCallback('image', mouse_callback)
+    cv2.imshow('image', img)
+    k = cv2.waitKey(0)
+    print(k)
+    # Press Enter
+    if(k == 13):
+        write_file(image_name, output)
+        '''
+        img = cv2.imread(path_image)
+        output = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(f'{IMAGE_DIR}{OUTPUT_FOLDER}/{image_name.split(".")[0]}.jpg', output)
+        '''
+    #cv2.waitKey(0)
+    # Press Space
+    if (k == 32):
+        cv2.destroyAllWindows()
+        break
